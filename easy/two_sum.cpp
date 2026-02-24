221cf7873774904a8d980f8b2542adf70a3481c0
SysEngRahul <rahul.upadhyay.etc@gmail.com>
2026-02-24 12:31:09 +0530

    Add Two Sum solution and update gitignore for build outputs

---
+++ easy/two_sum.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> nums = {2,7,11,15};
+    int target = 9;
+    unordered_map<int,int> mp;
+
+    for(int i=0;i<nums.size();i++){
+        int need = target - nums[i];
+        if(mp.count(need)){
+            cout << mp[need] << " " << i << endl;
+        }
+        mp[nums[i]] = i;
+    }
+}
